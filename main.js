b7d3cf2f384e06927581683cbd727fe907aeae55
Paulopsrj <paulopsrj92@gmail.com>
2022-04-19 21:03:53 -0300

    Format

--- main.js
+++ main.js
@@ -1,21 +1,20 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow } = require("electron");
 
-const path = require('path')
+const path = require("path");
 
-require('electron-reload')(__dirname, {
-  electron: path.join(__dirname, 'node_modules', '.bin', 'electron')
+require("electron-reload")(__dirname, {
+    electron: path.join(__dirname, "node_modules", ".bin", "electron"),
 });
 
-
 function criarJanela() {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    icon:__dirname+'/assets/icon.png'
-  })
- win.loadFile('index.html')
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        icon: __dirname + "/assets/icon.png",
+    });
+    win.loadFile("index.html");
 }
 
-app.whenReady().then(() =>{
-  criarJanela()
-})
+app.whenReady().then(() => {
+    criarJanela();
+});
